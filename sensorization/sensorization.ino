49b320fda5850a37cb4f21240c66f676a1dda611
Thyplock <brualgo@gmail.com>
2018-03-03 17:27:29 +0100

    All sensores working correctly!

--- sensorization/sensorization.ino
+++ sensorization/sensorization.ino
@@ -1,3 +1,15 @@
+//LIBRARIES
+#include <ESP8266WiFi.h>
+#include <string.h>
+
+//CONSTANTS
+const char* ssid = "hackathongi";
+const char* password = "tarla2018";
+const char* host = "84.89.60.4";
+const int httpPort = 80;
+const char* UNIQUE_ID = "sensors";
+const int loopDelay = 5000;
+
 //PINS
 const int readSensor = A0;
 const int photoresistorSensor = D0;
@@ -9,7 +21,7 @@ const int soundSensor = D5;
 
 //VARIABLES
 int photoresistorValue;
-int temperatureValue;
+float temperatureValue;
 int doorValue;
 int distanceValue;
 int soundValue;
@@ -17,6 +29,30 @@ int soundValue;
 //SETUP
 void setup() {
   Serial.begin(9600);
+  
+  /* We start by connecting to a WiFi network
+   *
+   * Explicitly set the ESP8266 to be a WiFi-client, otherwise, it by default,
+   * would try to act as both a client and an access-point and could cause
+   * network-issues with your other WiFi-devices on your WiFi-network. */
+  Serial.printf("Trying to connect %s\n", ssid);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+  }
+
+  if (WiFi.status() == WL_CONNECTED) {
+    Serial.println("Connection successfull!!!!!");
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
+    Serial.println("\n");
+  } else {
+    Serial.println("Connection successfull!!!!!");
+  }
+
+  //Define pin modes
   pinMode(photoresistorSensor, OUTPUT);
   pinMode(temperatureSensor, OUTPUT);
   pinMode(doorSensor, INPUT_PULLUP);
@@ -35,11 +71,15 @@ void loop() {
   readSound();
 
   //Print data
-  Serial.printf("Photoresistor: %d\n", photoresistorValue);
-  Serial.printf("Temperature: %d\n", temperatureValue);
+  Serial.printf("Photoresistor: %f\n", photoresistorValue);
+  Serial.printf("Temperature: %f\n", temperatureValue);
   Serial.printf("Door state: %d\n", doorValue);
   Serial.printf("Distance: %d\n", distanceValue);
   Serial.printf("Sound: %d\n\n", soundValue);
+
+  //Send to server
+  sendDataToServer(photoresistorValue, temperatureValue, doorValue, distanceValue, soundValue);
+  delay(loopDelay);
 }
 
 //FUNCTIONS
@@ -78,5 +118,52 @@ void readDistance () {
 
 void readSound() {
   soundValue = digitalRead(soundSensor);
+}
+
+void sendDataToServer(float photoresistor, float temperature, int doorState, int distance, int sound) {
+  Serial.println("Sending data to server....");
+
+  //Use WiFiClient class to create TCP connections
+  WiFiClient client;
+  if (!client.connect(host, httpPort)) {
+    Serial.println("Connection failed!!");
+    return;
+  }
+
+  char PostData[255];
+  sprintf(PostData, "{\"fotoresistencia\":{\"value\":%f,\"type\":\"Float\"}, \"temperatura\":{\"value\":%f,\"type\":\"Float\"},\"estatPorta\":{\"value\":%d,\"type\":\"Integer\"},\"distancia\":{\"value\":%d,\"type\":\"Integer\"},\"so\":{\"value\":%d,\"type\":\"Integer\"}}", photoresistor, temperature, doorState, distance, sound);
+  String Data = String(PostData);
+
+  Serial.println(Data);
+  
+  client.println("POST /v2/entities/" + String(UNIQUE_ID) + "/attrs HTTP/1.1");  
+  client.println("Content-Type: application/json");
+  client.println("Content-Length: " + String(Data.length()));
+  client.println();
+  client.println(Data);
+
+  unsigned long timeout = millis();
+  while (client.available() == 0) {
+    if (millis() - timeout > 10000) {
+      Serial.println(">>> Client Timeout !");
+      client.stop();
+      return;
+    }
+  }
+
+  //Read all the lines of the reply from server and print them to Serial
+  String response = "";
+  while (client.available()){
+    String line = client.readStringUntil('\r');
+    response += line;
+  }
+  if (response.indexOf("HTTP/1.1 204 No Content") != -1) {
+    Serial.println("OK");
+  } else {
+    Serial.print("KO: ");
+    Serial.println(response);
+  }
+  
+  Serial.println("Closing connection!");
 }
 
